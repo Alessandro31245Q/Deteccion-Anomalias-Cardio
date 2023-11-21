0d477d66fccf268c695ea0947f8ebb6b66ae50fb
Alessandro31245Q <143972373+Alessandro31245Q@users.noreply.github.com>
2023-11-21 04:38:50 -0500

    app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 ﻿import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from datetime import datetime
+#import pandas as pd
+#import numpy as np
+#import matplotlib.pyplot as plt
+#from datetime import datetime
 from utils import *
 
 #Caracteristicas basicas de la pagina
 st.set_page_config(page_icon="🤖", page_title="Proyecto_Deteccion", layout="wide")
-hora_actual = datetime.now().strftime("%H:%M:%S")
-st.subheader(f"Hora actual: {hora_actual}")
-st.title("Deteccion de anomalias en Series de Tiempo en Cardiografia")
+#hora_actual = datetime.now().strftime("%H:%M:%S")
+#st.subheader(f"Hora actual: {hora_actual}")
+#st.title("Deteccion de anomalias en Series de Tiempo en Cardiografia")
 
 c29, c30, c31 = st.columns([1, 6, 1]) # 3 columnas: 10%, 60%, 10%
 
@@ -40,21 +40,21 @@ with c30:
         datos_series_temporales = leer_dato(uploaded_file)
 
             # Mostrar la grafica estilo cardiograma
-        st.subheader("Cardiograma")
+        #st.subheader("Cardiograma")
 
             # Supongamos que tus datos tienen un formato de tiempo y valor
-        tiempo = np.arange(len(datos_series_temporales))
-        valores = datos_series_temporales
+        #tiempo = np.arange(len(datos_series_temporales))
+        #valores = datos_series_temporales
 
             # Crear un gráfico estilo cardiograma
-        fig, ax = plt.subplots(figsize=(10, 6))
-        ax.plot(tiempo, valores, color='blue', linewidth=2, label='Cardiograma')
-        ax.set_xlabel('Tiempo', fontsize=14)
-        ax.set_ylabel('Valor', fontsize=14)
-        ax.set_title('Representacion del Cardiograma', fontsize=16)
-        ax.legend()
-        ax.grid(True, linestyle='--', alpha=0.7)
-        st.pyplot(fig)
+       # fig, ax = plt.subplots(figsize=(10, 6))
+        #ax.plot(tiempo, valores, color='blue', linewidth=2, label='Cardiograma')
+        #ax.set_xlabel('Tiempo', fontsize=14)
+        #ax.set_ylabel('Valor', fontsize=14)
+        #ax.set_title('Representacion del Cardiograma', fontsize=16)
+        #ax.legend()
+        #ax.grid(True, linestyle='--', alpha=0.7)
+        #st.pyplot(fig)
 
             
         info_box_result = st.info(f"""
